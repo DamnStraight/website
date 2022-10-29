52c21f0f123c21c5f63bda57dd48af62735737c0
Steven Labelle <damnstraight@Stevens-Laptop.local>
2022-10-29 14:21:50 -0400

    Removed boilerplate, added some skeleton code

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="h-32 bg-slate-400">Footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Landing.tsx
@@ -0,0 +1,15 @@
+const Landing = () => {
+  return (
+    <section className="bg-gradient-to-r from-purple-500 to-pink-500">
+      <div className="container mx-auto h-screen flex flex-col justify-center items-center">
+        <div className="w-full text-center">
+          <p className="text-9xl">Steven Labelle</p>
+          <p className="text-4xl">Software Developer</p>
+          <p className="text-xl">Github | Linkedin</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
