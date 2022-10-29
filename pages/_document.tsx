ab1e442a263ee430735aeb9d833e026cd4cbe51c
Steven Labelle <damnstraight@Stevens-Laptop.local>
2022-10-29 14:22:51 -0400

    Added sass, formatting updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
