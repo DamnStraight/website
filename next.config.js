7a17e83d1002fce88b7968488f2989cb8952aec2
DamnStraight <ste77236@gmail.com>
2022-10-31 15:21:56 -0400

    Added config for github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  basePath: '/website',
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true
+  },
 }
 
 module.exports = nextConfig
