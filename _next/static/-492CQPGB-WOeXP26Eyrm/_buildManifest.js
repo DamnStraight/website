83db0133ae9bdff5b2c5a2d50f9d07226c658dd4
DamnStraight <ste77236@gmail.com>
2022-10-31 16:20:00 -0400

    Deploy

---
+++ _next/static/-492CQPGB-WOeXP26Eyrm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/828c4de1e6340e8a.css","static/chunks/pages/index-62a00eadd4140543.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
