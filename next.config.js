6ba8b557f7058d8a152051146af079f180013896
DamnStraight <ste77236@gmail.com>
2022-10-31 15:40:32 -0400

    More github pages configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
+  assetPrefix: isProd ? '/damnstraight.github.io/' : undefined,
   basePath: '/website',
   reactStrictMode: true,
   swcMinify: true,
