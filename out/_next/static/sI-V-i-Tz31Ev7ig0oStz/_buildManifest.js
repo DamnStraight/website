fc48a4017d839355b4d657908908fe530975e6e8
DamnStraight <ste77236@gmail.com>
2022-12-03 20:34:42 -0500

    Deploy

--- out/_next/static/sI-V-i-Tz31Ev7ig0oStz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e350a41e70edec94.css","static/chunks/pages/index-f88d98c1b8be2b05.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
