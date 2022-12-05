6ea1847416fb38ad580963b11d3821db36bcbde6
DamnStraight <ste77236@gmail.com>
2022-12-05 18:44:31 -0500

    Deploy

---
+++ _next/static/IhSR9WpSN5IBEiULFbZtE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a145e54f7471bf17.css","static/chunks/pages/index-98e5d6216da0a7f5.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/wyoaLP83flxjlPAfdyoRh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ef75b53c4dfbbf52.css","static/chunks/pages/index-5909fc52a5795778.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
